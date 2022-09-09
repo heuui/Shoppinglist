c2fc5a0e623e5e1399f08097f8722c6883aab9bf
heuui <henni.haavistola@gmail.com>
2022-09-09 10:21:00 +0300

    valmis tehtävä

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const addItem = () => {
+    setData([...data, { key: text }]);
+    setText('');
+  }
+
+  const clearItems = () => {
+    setData([]);
+    setText('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setText(text)} value={text}
+      />
+      <Button title="Add" onPress={addItem} />
+      <Button title="Clear" onPress={clearItems} />
+      <Text>Shopping list</Text>
+      <FlatList style={styles.list}
+        data={data}
+        renderItem={({ item }) =>
+        <Text>{item.key}</Text>
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +44,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    marginTop: 200,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
 });
+
